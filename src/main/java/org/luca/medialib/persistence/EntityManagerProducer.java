6be1010bf28d933d42352242a8165cb2c99521d7
luc4 <lucagraf@gmx.de>
2011-08-08 20:11:10 +0200

    * refactored EntityManagerProducer to not depend on EE-Resource Injection (@PersistenceContext) * removed now obsolete dependencies

--- src/main/java/org/luca/medialib/persistence/EntityManagerProducer.java
+++ src/main/java/org/luca/medialib/persistence/EntityManagerProducer.java
@@ -1,11 +1,18 @@
 package org.luca.medialib.persistence;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -13,11 +20,34 @@ import javax.persistence.PersistenceContext;
  * 
  * @author luc4
  */
-@RequestScoped
+@ApplicationScoped
 public class EntityManagerProducer
 {
-	@PersistenceContext( unitName = "mediaLibPU" )
-	private EntityManager entityManager;
+	private static final Logger log = LoggerFactory.getLogger( EntityManagerProducer.class );
+
+	private EntityManagerFactory emf;
+
+
+	@PostConstruct
+	private void initialize()
+	{
+		log.debug( "Create EntityManagerFactory ..." );
+		emf = Persistence.createEntityManagerFactory( "mediaLibPU" );
+		log.debug( "EntityManagerFactory succesfully created [{}][opened: {}]", emf, emf.isOpen() );
+	}
+
+
+	@PreDestroy
+	private void release()
+	{
+		log.debug( "Releasing EntityManagerFactory ..." );
+		if ( null != emf && emf.isOpen() )
+		{
+			emf.close();
+			log.debug( "EntityManagerFactory succesfully closed [{}][opened: {}]", emf,
+					emf.isOpen() );
+		}
+	}
 
 
 	@Produces
@@ -25,15 +55,20 @@ public class EntityManagerProducer
 	@Default
 	public EntityManager createDefaultEntityManager()
 	{
-		return entityManager;
+		log.debug( "Create new @RequestScoped EntityManger ..." );
+		final EntityManager em = emf.createEntityManager();
+		log.debug( "EntityManger successfully created [{}][opened: {}]!", em, em.isOpen() );
+		return em;
 	}
 
 
-	public void dispose( @Disposes @Default EntityManager entityManager )
+	public void dispose( @Disposes @Default EntityManager em )
 	{
-		if ( entityManager.isOpen() )
+		log.debug( "Closing @RequestScoped EntityManger ..." );
+		if ( em.isOpen() )
 		{
-			entityManager.close();
+			em.close();
+			log.debug( "EntityManger successfully closed [{}][opened: {}]!", em, em.isOpen() );
 		}
 	}
 }
